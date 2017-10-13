b6235f20ac49ede5d14d0544ea0f4664dd5a8894
Jared Howard <jared@howardpants.com>
2017-10-13 14:02:55 -0600

    filtering webRequests to our tracking domains

--- background.js
+++ background.js
@@ -1,14 +1,23 @@
 'use strict';
 
-chrome.webNavigation.onCompleted.addListener(function(){
-    //alert('hello')
-});
+//chrome.webNavigation.onCompleted.addListener(function(){
+//    //alert('hello')
+//});
 
-var callback = function(details) {
-    alert(details.url);
-};
-var filter = {urls: ["<all_urls>"]};
-var opt_extraInfoSpec = [];
+chrome.webRequest.onCompleted.addListener(function(req) {
+    var re = /type=([^&])/
+    var found = req.url.match(re)
 
-chrome.webRequest.onBeforeRequest.addListener(
-    callback, filter, opt_extraInfoSpec);
+    if (req.url.match(/\/imp\?/)) {
+        alert("Impression Fired!");
+    } else if (req.url.match(/\/eng\?/)) {
+        alert("Engagement Fired!");
+    } else if (req.url.match(/\/act\?/) && found) {
+        alert("Action " + found[1] + " Fired!");
+    } else {
+        alert("Tracking Fired!\n" + req.url);
+    }
+}, {urls: [
+    "*://tags.rd.linksynergy.com/*",
+    "*://tags.mediaforge.com/*",
+]}, []);
