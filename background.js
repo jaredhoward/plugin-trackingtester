65f0f5a34006830d9556b56522bb922c38ef927b
Jared Howard <jared@howardpants.com>
2017-10-13 13:22:52 -0600

    webRequest working with annoying alert

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
+'use strict';
+
 chrome.webNavigation.onCompleted.addListener(function(){
     //alert('hello')
 });
+
+var callback = function(details) {
+    alert(details.url);
+};
+var filter = {urls: ["<all_urls>"]};
+var opt_extraInfoSpec = [];
+
+chrome.webRequest.onBeforeRequest.addListener(
+    callback, filter, opt_extraInfoSpec);
